c253cf0d89e35e998a7775a8aab4a68dec4c64d0
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-11-27 20:29:43 +0100

    Update app.js

--- Renderer/app.js
+++ Renderer/app.js
@@ -1,43 +1,88 @@
 const { desktopCapturer } = require('electron')
 
-document.getElementById('record').addEventListener("click", function(){ console.log("record")});
-document.getElementById('stop').addEventListener("click", function(){ console.log("stop")});
-const constraints = {
-  audio: {
-    mandatory: {
-      chromeMediaSource: 'desktop'
-    }
-  },
-  video: {
-    mandatory: {
-      chromeMediaSource: 'desktop',
-      minWidth: 400,
-      maxWidth: 400,
-      minHeight: 400,
-      maxHeight: 400
-    }
-  }
-}
-desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
-  for (const source of sources) {
-    if (source.name === 'Screen Recorder') {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints)
-        handleStream(stream)
-      } catch (e) {
-        handleError(e)
+document.getElementById('record').addEventListener("click", function(){recordVideo();});
+document.getElementById('stop').addEventListener("click", function(){stopVideo();});
+
+
+function recordVideo(){
+  const constraints = {
+    audio: {
+      mandatory: {
+        chromeMediaSource: 'desktop'
       }
-      return
+    },
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        minWidth: 600,
+        maxWidth: 600,
+        minHeight: 400,
+        maxHeight: 400
+      }
     }
   }
-})
-
-function handleStream (stream) {
-  const video = document.querySelector('video')
-  video.srcObject = stream
-  video.onloadedmetadata = (e) => video.play()
+  desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
+    for (const source of sources) {
+      if (source.name === 'Screen Recorder') {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia(constraints)
+          handleStream(stream)
+        } catch (e) {
+          handleError(e)
+        }
+        return
+      }
+    }
+  })
+  
+  function handleStream (stream) {
+    const video = document.querySelector('video')
+    video.srcObject = stream
+    video.onloadedmetadata = (e) => video.play()
+  }
+  
+  function handleError (e) {
+    console.log(e)
+  }
 }
-
-function handleError (e) {
-  console.log(e)
+function stopVideo(){
+  const constraints = {
+    audio: {
+      mandatory: {
+        chromeMediaSource: 'desktop'
+      }
+    },
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        minWidth: 600,
+        maxWidth: 600,
+        minHeight: 400,
+        maxHeight: 400
+      }
+    }
+  }
+  desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
+    for (const source of sources) {
+      if (source.name === 'Screen Recorder') {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia(constraints)
+          handleStream(stream)
+        } catch (e) {
+          handleError(e)
+        }
+        return
+      }
+    }
+  })
+  
+  function handleStream (stream) {
+    const video = document.querySelector('video')
+    video.srcObject = stream
+    video.onloadedmetadata = (e) => video.pause()
+  }
+  
+  function handleError (e) {
+    console.log(e)
+  }
 }
