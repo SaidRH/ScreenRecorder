d13c7448f08177715acaeb1d3ed3e592147c582a
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-11-27 19:26:23 +0100

    Update app.js

--- Main/app.js
+++ Main/app.js
@@ -2,12 +2,12 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 300,
-    height: 150,
-    maxHeight:150,
-    maxWidth:300,
-    minHeight:150,
-    minWidth:300,
+    width: 1000,
+    height: 600,
+    maxHeight:1000,
+    maxWidth:600,
+    minHeight:1000,
+    minWidth:600,
     frame:false,
     transparent:true,
     icon:"Renderer/images/icon.png",
@@ -16,6 +16,7 @@ function createWindow () {
     }
   })
   win.loadFile('Renderer/index.html')
+  win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
