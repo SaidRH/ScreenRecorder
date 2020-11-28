2d679abf4832e72768b421d7d1c2d1305b04ad6e
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-11-28 13:30:13 +0100

    Update app.js

--- Main/app.js
+++ Main/app.js
@@ -4,12 +4,6 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 1000,
     height: 600,
-    maxHeight:1000,
-    maxWidth:600,
-    minHeight:1000,
-    minWidth:600,
-    frame:true,
-    transparent:true,
     icon:"Renderer/images/icon.png",
     webPreferences: {
       nodeIntegration: true
