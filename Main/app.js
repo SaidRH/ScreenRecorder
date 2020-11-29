2d03c0897118696f8473bb2716d0d8630c976d7b
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-11-29 16:00:39 +0100

    Update app.js

--- Main/app.js
+++ Main/app.js
@@ -1,34 +1,36 @@
-const { app, BrowserWindow } = require('electron')
+const {
+    app,
+    BrowserWindow
+} = require('electron')
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 300,
-    height: 150,
-    maxHeight: 150,
-    maxWidth: 300,
-    minHeight: 150,
-    minWidth: 300,
-    frame: false,
-    transparent: true,
-    icon: "Renderer/images/icon.png",
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation:true
-    }
-  })
-  win.loadFile('Renderer/index.html')
+    const win = new BrowserWindow({
+        width: 300,
+        height: 150,
+        maxHeight: 150,
+        maxWidth: 300,
+        minHeight: 150,
+        minWidth: 300,
+        frame: false,
+        transparent: true,
+        icon: "Renderer/images/icon.png",
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadFile('Renderer/index.html')
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
